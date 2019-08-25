8ac0a0726074d733459d837126ba1a8c9fed6c56
Reno McKenzie <Reno@RenoMcKenzie.com>
2019-08-24 22:52:33 -0700

    cleanup and notes

--- src/utils/uncompress.js
+++ src/utils/uncompress.js
@@ -1,6 +1,30 @@
 const LZString = require('../vendor/lz-string');
 
-// todo: add safeguards. removed from lz-string
+/*  ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
+"P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "[", "\", "]", "^", "_",
+"`", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
+"p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+58 max blocks atm. extentable to 62 without rework */
+const LOOKUP_TABLE = new Array(58).fill(1).map((_,i)=>String.fromCharCode(65+i));
+const WILDCARD = '-';
+
+function mapStringToArr(str) {
+  // construct 3d array from string
+    // idea: moving slicer from start to end to cut Z's and place in proper XY grid.
+
+  for (let i=0; i<str.length; i++) {
+    // const block = (
+    //   str[i] === WILDCARD
+    //     ? WILDCARD
+    //     : (LOOKUP_TABLE.indexOf(str[i]) || WILDCARD)
+    // );
+
+
+  }
+
+  // create map from seed
+  // apply our stringMapArr on top of seed map
+}
 
 function repeat(char, times) {
   let out = '';
@@ -9,7 +33,6 @@ function repeat(char, times) {
 }
 
 function unMinify(str) {
-  console.log('unMinify', !!str && str)
   let out = ''
 
   let numberAssembly = '';
@@ -26,15 +49,14 @@ function unMinify(str) {
 }
 
 function main() {
-  // const mapStr = /*@__PURE__*/ mapArrToString();
-  // const mapStrMinified = /*@__PURE__*/ minifyRepeats(mapStr);
-  // const compressed = LZString.compress(mapStrMinified);
+  // const seed = window.localStorage.get
+  const seed = 421;
   const compressed = window.localStorage.getItem('_mcm');
 
-  // window.localStorage.setItem('_mcm', compressed);
   const uncompressed = LZString.decompress(compressed);
-  const unMinified = unMinify(uncompressed);
-  console.log(unMinified.length);
+  const mapStr = unMinify(uncompressed);
+  const mapArr = mapStringToArr(mapStr);
+  console.log(mapArr);
 }
 
 module.exports = {
